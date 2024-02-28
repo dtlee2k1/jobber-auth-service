428f48e804b01107fcedb58932768fc3edfa73eb
Le Duc Thai <leducthai2001@gmail.com>
2024-02-29 01:15:27 +0700

    create schemas validation

---
+++ src/schemes/signin.ts
@@ -0,0 +1,25 @@
+import Joi, { ObjectSchema } from 'joi';
+
+const loginSchema: ObjectSchema = Joi.object().keys({
+  username: Joi.alternatives().conditional(Joi.string().email(), {
+    then: Joi.string().email().required().messages({
+      'string.base': 'Email must be of type string',
+      'string.email': 'Invalid email',
+      'string.empty': 'Email is a required field'
+    }),
+    otherwise: Joi.string().min(4).max(12).required().messages({
+      'string.base': 'Username must be of type string',
+      'string.min': 'Username length must be from 4 to 12 characters',
+      'string.max': 'Username length must be from 4 to 12 characters',
+      'string.empty': 'Username is a required field'
+    })
+  }),
+  password: Joi.string().min(6).max(32).required().messages({
+    'string.base': 'Password must be of type string',
+    'string.min': 'Password length must be from 6 to 32 characters',
+    'string.max': 'Password length must be from 6 to 32 characters',
+    'string.empty': 'Password is a required field'
+  })
+});
+
+export { loginSchema };
---
+++ src/schemes/signup.ts
@@ -0,0 +1,36 @@
+import Joi, { ObjectSchema } from 'joi';
+
+const signupSchema: ObjectSchema = Joi.object().keys({
+  username: Joi.string().min(4).max(12).required().messages({
+    'string.base': 'Username must be of type string',
+    'string.min': 'Username length must be from 4 to 12 characters',
+    'string.max': 'Username length must be from 4 to 12 characters',
+    'string.empty': 'Username is a required field',
+    'any.required': 'Username is a required field'
+  }),
+  password: Joi.string().min(6).max(32).required().messages({
+    'string.base': 'Password must be of type string',
+    'string.min': 'Password length must be from 6 to 32 characters',
+    'string.max': 'Password length must be from 6 to 32 characters',
+    'string.empty': 'Password is a required field',
+    'any.required': 'Password is a required field'
+  }),
+  country: Joi.string().required().messages({
+    'string.base': 'Country must be of type string',
+    'string.empty': 'Country is a required field',
+    'any.required': 'Country is a required field'
+  }),
+  email: Joi.string().email().required().messages({
+    'string.base': 'Email must be of type string',
+    'string.email': 'Invalid email',
+    'string.empty': 'Email is a required field',
+    'any.required': 'Email is a required field'
+  }),
+  profilePicture: Joi.string().required().messages({
+    'string.base': 'Please add a profile picture',
+    'string.empty': 'Profile picture is required',
+    'any.required': 'Profile picture is required'
+  })
+});
+
+export { signupSchema };
